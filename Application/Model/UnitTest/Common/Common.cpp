c5af331e9f988966553bdc6ecf39582a75d68bfa
tangyingzhong <tangyz114987@outlook.com>
2020-05-07 21:16:22 +0800

    Add fundation plugin unit test

---
+++ Application/Model/UnitTest/Common/Common.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Common
+{
+	TEST_CLASS(Common)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
